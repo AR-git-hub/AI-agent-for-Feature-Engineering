c57ffef60f05676707a8685b836db64f3b6a149d
Рябков Александр Владиславович <103053621+AR-git-hub@users.noreply.github.com>
2026-04-12 13:55:06 +0300

    Delete src directory

--- src/mas/domain/__init__.py
+++
@@ -1,4 +0,0 @@
-from mas.domain.models import EDASummary, FeatureConfig
-from mas.domain.state import PipelineState
-
-__all__ = ["EDASummary", "FeatureConfig", "PipelineState"]
--- src/mas/pipeline/__init__.py
+++
@@ -1,3 +0,0 @@
-from mas.pipeline.submission import main
-
-__all__ = ["main"]
